ff5848647286f10fc48f384b8c6c83034ca6dbde
Ketlez <88856386+Ketlez@users.noreply.github.com>
2022-07-21 17:55:28 +0300

    Много кубиков

--- src/main.cpp
+++ src/main.cpp
@@ -108,6 +108,18 @@ int main()
         1, 2, 3
     };
 
+    glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f,  0.0f,  0.0f),
+    glm::vec3(2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f,  2.0f, -2.5f),
+    glm::vec3(1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
 
     VAO vao;
     VBO vbo;
@@ -169,21 +181,31 @@ int main()
         vao.bind();
 
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        
 
         glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
 
         glm::mat4 projection = glm::mat4(1.0f);
         projection = glm::perspective(glm::radians(45.0f), (float)w / (float)h, 0.01f, 100.0f);
 
-        shaderProgram.setMatrix("model", model);
+        
         shaderProgram.setMatrix("view", view);
         shaderProgram.setMatrix("projection", projection);
 
 
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle)+ (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+            shaderProgram.setMatrix("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        
        
         glfwSwapBuffers(window);
         glfwPollEvents();
